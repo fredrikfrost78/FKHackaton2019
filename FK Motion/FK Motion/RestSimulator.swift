de15aa94f2a20e8626f13823504b666a6aa4365a
Fredrik Frost <fredrikfrost@hotmail.com>
2019-05-16 16:26:43 +0200

    Lagt till restsimulator

---
+++ FK Motion/FK Motion/RestSimulator.swift
@@ -0,0 +1,13 @@
+//
+//  RestSimulator.swift
+//  FK Motion
+//
+//  Created by Fredrik Frost on 2019-05-16.
+//  Copyright © 2019 SuperNova. All rights reserved.
+//
+
+import Foundation
+
+class RestSimulator {
+    
+}
